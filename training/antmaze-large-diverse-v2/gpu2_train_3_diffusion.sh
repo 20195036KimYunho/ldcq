9aaa71786646a7b56d1d5b7c5891870246bd081e
20195036KimYunho <dbsgh797210@gist.ac.kr>
2024-04-30 09:48:22 +0000

    add_ant_maze_large

---
+++ training/antmaze-large-diverse-v2/gpu2_train_3_diffusion.sh
@@ -0,0 +1,21 @@
+CUDA_VISIBLE_DEVICES=2 python ./train_diffusion.py \
+    --env antmaze-large-diverse-v2  \
+    --device 'cuda' \
+    --n_epoch 300 \
+    --lrate 1e-4 \
+    --batch_size 32 \
+    --net_type 'unet' \
+    --n_hidden 512 \
+    --test_split 0.2 \
+    --sample_z 0 \
+    --checkpoint_dir 'ldcq/ldcq/checkpoints' \
+    --dataset_dir 'ldcq/ldcq/data' \
+    --skill_model_filename 'skill_model_antmaze-large-diverse-v2_encoderType(gru)_state_dec_none_policy_dec_autoregressive_H_30_b_0.05_conditionalp_1_zdim_16_adist_normal_testSplit_0.2_separatetest_0_getrewards_1_appendgoals_0_best.pth' \
+    --append_goals 0 \
+    --drop_prob 0.1 \
+    --diffusion_steps 500 \
+    --cfg_weight 0.0 \
+    --predict_noise 0 \
+    --normalize_latent 0 \
+    --schedule 'linear'
+    
