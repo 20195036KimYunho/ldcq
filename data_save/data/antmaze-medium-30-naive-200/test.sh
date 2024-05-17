a3ebc0fbdc8a9731fdbba46c81c4ed1249d5aaa3
20195036KimYunho <dbsgh797210@gist.ac.kr>
2024-05-17 06:59:39 +0000

    delete_unecessary

--- data_save/data/antmaze-medium-30-naive-200/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *skill_model*.npy; do
-	    mv "$file" "$(echo "$file" | sed 's/antmaze-medium-30-naive-200skill_model/skill_model/')"
-    done
-
--- notuse/autosh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sh maze2d.sh
-
-sh antmaze.sh
-
-sh halfcheetah.sh
