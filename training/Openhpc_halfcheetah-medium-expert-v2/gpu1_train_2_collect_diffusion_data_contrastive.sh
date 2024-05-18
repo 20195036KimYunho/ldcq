677f4b6619abe309adfd6433d27a0473047c2f30
jaehyun00518 <jaehyun00518@gmail.com>
2024-05-18 05:47:44 +0000

    add_openhpc_setting

---
+++ training/Openhpc_halfcheetah-medium-expert-v2/gpu1_train_2_collect_diffusion_data_contrastive.sh
@@ -0,0 +1,26 @@
+CUDA_VISIBLE_DEVICES=1 python ./collect_diffusion_data.py \
+    --env halfcheetah-medium-expert-v2 \
+    --device 'cuda' \
+    --skill_model_filename 'halfcheetah-medium-expert-v2_H_20_adist_softmax_use_contrastive_1_num_categorical_interval_10_contrastive_ratio_0.05_getrewards_1_appendgoals_0_best.pth'\
+    --batch_size 1024 \
+    --append_goals 0 \
+    --save_z_dist 0 \
+    --get_rewards 1 \
+    --horizon 20 \
+    --stride 1 \
+    --beta 0.1 \
+    --a_dist softmax\
+    --encoder_type gru\
+    --state_decoder_type mlp\
+    --policy_decoder_type autoregressive\
+    --per_element_sigma 1\
+    --conditional_prior 1\
+    --h_dim 256\
+    --z_dim 16\
+    --checkpoint_dir '/workspace/ldcq/ldcq/checkpoints/contrastive-halfcheetah-medium-expert-20'\
+    --dataset_dir '/workspace/ldcq/ldcq/data/contrastive-halfcheetah-medium-expert-20' \
+    --use_contrastive 1 \
+    --contrastive_ratio 0.05 \
+    --num_categorical_interval 10 \
+    --append_goals 0 \
+    --horizon 20
---
+++ training/Openhpc_walker2d-medium-expert-v2/gpu1_train_1_skill_model_contrastive.sh
@@ -0,0 +1,26 @@
+CUDA_VISIBLE_DEVICES=1 python ./train_skills.py \
+    --env walker2d-medium-expert-v2 \
+    --train_diffusion_prior 0 \
+    --z_dim 16 \
+    --lr 5e-5 \
+    --separate_test_trajectories 0 \
+    --test_split 0.0 \
+    --get_rewards 1 \
+    --num_epochs 200 \
+    --start_training_state_decoder_after 201 \
+    --normalize_latent 0 \
+    --policy_decoder_type autoregressive \
+    --state_decoder_type mlp \
+    --a_dist softmax \
+    --beta 0.01 \
+    --conditional_prior 1 \
+    --checkpoint_dir '/home/jovyan/beomi/jaehyun/ldcq/checkpoints/gpu1_walker2d-medium-expert' \
+    --dataset_dir '/home/jovyan/beomi/jaehyun/ldcq/data/gpu1_walker2d-medium-expert' \
+    --use_contrastive 1 \
+    --contrastive_ratio 0.4 \
+    --num_categorical_interval 10 \
+    --append_goals 0 \
+    --horizon 10 \
+    --margin 0.4 \
+    --scale 30
+
